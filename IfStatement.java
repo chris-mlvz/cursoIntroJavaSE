4360c4f0a6d0dcb9ac4aff7dc40ccbf6c855d425
chris_mlvz <chris.mlvz@gmail.com>
2021-06-21 14:24:00 -0500

    chaged file if

--- IfStatement.java
+++ IfStatement.java
@@ -1,11 +1,7 @@
 public class IfStatement {
     public static void main(String[] args) {
         boolean isBluetoothEnabled = true;
-        int fileSended = 3;
-
         if (isBluetoothEnabled) {
-            // Send file
-            fileSended++;
             System.out.println("Archivo Enviado");
         }
     }
