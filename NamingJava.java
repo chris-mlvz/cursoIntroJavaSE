ef3486251fea2726c12ab99721c1760fe33f7b36
chris_mlvz <chris.mlvz@gmail.com>
2021-06-21 11:28:22 -0500

    camelCase

--- NamingJava.java
+++ NamingJava.java
@@ -1,3 +1,4 @@
+// Upper Camel Case
 public class NamingJava {
     public static void main(String[] args) {
         int cellphone = 33337777;
@@ -14,5 +15,9 @@ public class NamingJava {
         // final int POSITION = -5;
         // final int MAX_WIDTH = 9999;
         // final int MIN_WIDTH = 1;
+
+        // Lower Camel Case
+        // String fullName = "Irene Anahí Slagado Días de la Vega";
+        // int sizeInCentimeters = 26;
     }
 }
