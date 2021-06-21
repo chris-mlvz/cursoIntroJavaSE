a1c061f6dc0bc9a152e197cd6ca1b3a6c183bb72
chris_mlvz <chris.mlvz@gmail.com>
2021-06-21 14:30:11 -0500

    switch

---
+++ Switch.java
@@ -0,0 +1,20 @@
+public class Switch {
+    public static void main(String[] args) {
+        String colorModeSelected = "Dark";
+
+        switch (colorModeSelected) {
+            case "Light":
+                System.out.println("Seleccionaste Light Mode");
+                break;
+            case "Night":
+                System.out.println("Seleccionaste Night Mode");
+                break;
+            case "Dark":
+                System.out.println("Seleccionaste Dark Mode");
+                break;
+            default:
+                System.out.println("Selecciona una opción correcta");
+                break;
+        }
+    }
+}
